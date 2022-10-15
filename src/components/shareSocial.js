99a93ba1d3aba72b04f51ced62aca0bc558ad35c
Marta Blaszczak <47250336+MaraBl@users.noreply.github.com>
2022-10-16 00:04:25 +0100

    add share social component

---
+++ src/components/shareSocial.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ShareSocial = () => {
+  return (
+    <div className="shareSocial">
+      <p>
+        <i className="fa fa-share-alt" aria-hidden="true"></i>Share
+      </p>
+      <a href="#" className="fa fa-envelope"></a>
+      <a href="#" className="fa fa-twitter"></a>
+    </div>
+  );
+};
+
+export default ShareSocial;
